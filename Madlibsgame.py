660a9a7ad00776cc261ac1f017e7bcde584754f1
monkmonday <mob850580@gmail.com>
2026-02-08 18:11:28 +0530

    Add files via upload

---
+++ Madlibsgame.py
@@ -0,0 +1,14 @@
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter another adjective: ") 
+animal1 = input("Enter an animal: ")
+verb1 = input("Enter a verb: ")
+adverb1 = input("Enter an adverb: ")
+adjective3 = input("Enter another adjective: ")
+noun1 = input("Enter a noun: ")
+adjective4 = input("Enter one more adjective: ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"I saw a {adjective2} {animal1} jumping up and down in its tree.")
+print(f"He {verb1} {adverb1} through the large tunnel that led to its {adjective3} {noun1}.")
+print(f"I was {adjective4}!")
+
