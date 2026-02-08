f87a8f9606bca453699122d7c8b2d46767d67c14
Mohit <jhamohit1216@gmail.com>
2026-02-08 16:45:47 +0530

    Initial Python practice programs

---
+++ main.py
@@ -0,0 +1,13 @@
+name = input('What is your name? : ')
+age = int(input('How Old Are you? : '))
+
+
+    
+
+
+print(f'Hello {name}')
+print(f"{name} Lets Check Your Voting Eligibility:")
+if age >=18 :
+    print("You can Vote")
+else:
+    print("Try next Year")
